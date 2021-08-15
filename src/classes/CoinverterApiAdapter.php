d8eb133c0802bb110f799e54c70270e6a0c0af30
flyinter <120586558+flyinter@users.noreply.github.com>
2021-08-15 12:17:05 +0000

    reset

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -28,4 +28,4 @@ class CoinverterApiAdapter implements Coinverter
      */
     public function __construct(Client $client = null, string $apiKey = null)
     {
-        $this->API_KEY = $apiKey ?? config('coinverter.currencyconverte
+        $this->API_KEY = $apiKey ?? config('coinverter.currencyconverterapi.api-key'
