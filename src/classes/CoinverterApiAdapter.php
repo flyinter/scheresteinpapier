ea68c64d289600d290c633326d2fc3d200be204b
flyinter <120586558+flyinter@users.noreply.github.com>
2021-08-30 18:00:55 +0000

    v0.10.1

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -45,4 +45,4 @@ class CoinverterApiAdapter implements Coinverter
             return 'free';
         }
 
-        if (config('coinverter.currencyconverterapi.account-type')
+        if (config('coinverter.currencyconverterapi.account-type') == 'pr
