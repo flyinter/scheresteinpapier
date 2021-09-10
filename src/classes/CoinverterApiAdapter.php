80deaac31534d89485746380ee65ff4f10a0879a
flyinter <120586558+flyinter@users.noreply.github.com>
2021-09-10 14:07:19 +0000

    hhhh

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -58,4 +58,4 @@ class CoinverterApiAdapter implements Coinverter
      */
     private function determineBaseUrl()
     {
-        if (config('coinverter.currencyc
+        if (config('coinverter.currencyconver
