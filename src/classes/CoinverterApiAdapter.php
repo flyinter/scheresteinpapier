a78e2832abeeddffc02d020eefdcecd86c8f4e04
flyinter <120586558+flyinter@users.noreply.github.com>
2021-09-11 06:12:13 +0000

    updated Lithuanian translation

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -58,4 +58,4 @@ class CoinverterApiAdapter implements Coinverter
      */
     private function determineBaseUrl()
     {
-        if (config('coinverter.currencyconverterapi.accou
+        if (config('coinverter.currencyconverterapi.account-type'
