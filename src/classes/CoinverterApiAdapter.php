07302a88b25a4356499b42cc69dd059169531b65
flyinter <120586558+flyinter@users.noreply.github.com>
2021-09-11 08:27:15 +0000

    Re-bootstrapped the package.

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -58,4 +58,4 @@ class CoinverterApiAdapter implements Coinverter
      */
     private function determineBaseUrl()
     {
-        if (config('coinverter.currencyconverterapi.account-type'
+        if (config('coinverter.currencyconverterapi.account-type') == 'free'
