167c3555bb2d28d6e6ed8766a5ef703871a755fe
flyinter <120586558+flyinter@users.noreply.github.com>
2021-09-15 21:13:18 +0000

    Small updates.

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -62,4 +62,4 @@ class CoinverterApiAdapter implements Coinverter
             return 'https://free.currconv.com/api/v7';
         }
 
-        if (config('coinverter.currencyconverterap
+        if (config('coinverter.currencyconverterapi.account
