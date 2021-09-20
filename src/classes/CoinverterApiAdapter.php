33080e153c739a9547eb277ae248ac19782c00f1
flyinter <120586558+flyinter@users.noreply.github.com>
2021-09-20 05:36:07 +0000

    Test CI

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -63,4 +63,5 @@ class CoinverterApiAdapter implements Coinverter
         }
 
         if (config('coinverter.currencyconverterapi.account-type') == 'pro') {
-            return 'https://api.currconv.com/api/v7
+            return 'https://api.currconv.com/api/v7';
+    
