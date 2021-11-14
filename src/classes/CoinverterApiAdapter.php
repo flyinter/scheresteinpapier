0660629c2dd95627a6c7d5a9eb3e4b08c1837b59
flyinter <120586558+flyinter@users.noreply.github.com>
2021-11-14 19:30:22 +0000

    staging: comedi: dt282x: dt282x_ai_insn_read() always fails

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -120,4 +120,4 @@ class CoinverterApiAdapter implements Coinverter
 
         return $this->makeRequest('/convert', [
             'q'       => $conversion,
-            'date'    => $date->forma
+            'date'    => $date->format('Y-m-
