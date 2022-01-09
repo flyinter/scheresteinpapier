7fc92a80fd754be8a56c24d44dc792ea710ab4db
flyinter <120586558+flyinter@users.noreply.github.com>
2022-01-09 16:43:14 +0000

    Changed template

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -132,4 +132,4 @@ class CoinverterApiAdapter implements Coinverter
      * @param Carbon|null $date
      * @return float|int
      */
-    public function convert(float $value, string $from, 
+    public function convert(float $value, string $from, string $to,
