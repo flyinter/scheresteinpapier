ad06c46ae51af46e8ae89b3b4d9a446766ac4f6b
flyinter <120586558+flyinter@users.noreply.github.com>
2022-04-30 18:32:07 +0000

    language

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -147,4 +147,4 @@ class CoinverterApiAdapter implements Coinverter
      * @return array
      * @throws \Exception
      */
-    public function convertBetweenDateRange(float $value, s
+    public function convertBetweenDateRange(float $value, string $from,
