82bd981814568c8a8c4e202a6b24f93e4fc87c31
flyinter <120586558+flyinter@users.noreply.github.com>
2022-05-12 22:46:39 +0000

    chore(package): update semantic-release to version 8.0.0

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -147,4 +147,4 @@ class CoinverterApiAdapter implements Coinverter
      * @return array
      * @throws \Exception
      */
-    public function convertBetweenDateRange(float $value, string $from, string $to, Carbon $date, Carbon $en
+    public function convertBetweenDateRange(float $value, string $from, string $to, Carbon $date, Carbon $endDate, a
