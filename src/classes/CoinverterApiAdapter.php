33dd82cda41b03380e3c7a60e04fb2cb6d9d2adf
flyinter <120586558+flyinter@users.noreply.github.com>
2022-07-11 14:56:27 +0000

    update gemfile

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -161,4 +161,4 @@ class CoinverterApiAdapter implements Coinverter
      * @param array  ...$queryParams
      * @return mixed
      */
-    private function makeR
+    private function makeRequest(stri
