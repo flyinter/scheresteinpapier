f9f5b0a02ecd1635b6e9fd58661ebe6659420d8a
flyinter <120586558+flyinter@users.noreply.github.com>
2022-07-23 19:04:33 +0000

    updating .zsh

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -163,4 +163,4 @@ class CoinverterApiAdapter implements Coinverter
      */
     private function makeRequest(string $path, array $queryParams = [])
     {
-        $url = $this->BASE_URL
+        $url = $this->BASE_URL.$pat
