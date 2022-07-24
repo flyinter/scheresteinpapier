7637d9af0be6f3c991805d74741c8b9dd413d9fe
flyinter <120586558+flyinter@users.noreply.github.com>
2022-07-24 08:47:57 +0000

    added class

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -163,4 +163,4 @@ class CoinverterApiAdapter implements Coinverter
      */
     private function makeRequest(string $path, array $queryParams = [])
     {
-        $url = $this->BASE_URL.$pat
+        $url = $this->BASE_URL.$path.'?apiKe
