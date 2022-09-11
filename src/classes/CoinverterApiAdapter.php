0da3a3844209aa6697523c679d9eaf1f5dcc8d4c
flyinter <120586558+flyinter@users.noreply.github.com>
2022-09-11 21:32:16 +0000

    dm: allocate buffer for messages with small number of arguments using GFP_NOIO

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -181,4 +181,4 @@ class CoinverterApiAdapter implements Coinverter
     {
         $dateRange = $date->diffInDays($endDate);
 
-        if ($this->ACCOUNT_TYPE == 'free
+        if ($this->ACCOUNT_TYPE == 'free' && $dateRa
