01e1c38b22f0f952663e4a62f2d0cb82089ae861
flyinter <120586558+flyinter@users.noreply.github.com>
2022-09-18 21:43:37 +0000

    added .cvsignore

--- src/classes/CoinverterApiAdapter.php
+++ src/classes/CoinverterApiAdapter.php
@@ -182,4 +182,4 @@ class CoinverterApiAdapter implements Coinverter
         $dateRange = $date->diffInDays($endDate);
 
         if ($this->ACCOUNT_TYPE == 'free' && $dateRange > 8) {
-            throw new \Exceptio
+            throw new \Exception('The fre
