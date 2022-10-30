e939164c49b95e949b115df0c879c96c2249b95e
flyinter <120586558+flyinter@users.noreply.github.com>
2022-10-30 15:37:01 +0000

    Fix readme markdown

--- src/classes/ExchangeRatesApiAdapter.php
+++ src/classes/ExchangeRatesApiAdapter.php
@@ -1,2 +1,15 @@
 
 <?php
+
+namespace AshAllenDesign\Coinverter;
+
+use AshAllenDesign\Coinverter\Contracts\Coinverter;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+
+class ExchangeRatesApiAdapter implements Coinverter
+{
+    /** @var string */
+    private $BASE_URL;
+
+    /** @var Client */
