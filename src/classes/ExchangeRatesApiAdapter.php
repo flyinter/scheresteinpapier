f6411bea05fc5f9209610bc685751915fa415748
flyinter <120586558+flyinter@users.noreply.github.com>
2022-11-05 08:43:58 +0000

    Bump version to v1.2.0

--- src/classes/ExchangeRatesApiAdapter.php
+++ src/classes/ExchangeRatesApiAdapter.php
@@ -16,3 +16,17 @@ class ExchangeRatesApiAdapter implements Coinverter
     private $client;
 
     /**
+     * CoinverterApiAdapter constructor.
+     * @param Client|null $client
+     */
+    public function __construct(Client $client = null)
+    {
+        $this->BASE_URL = 'https://api.exchangeratesapi.io';
+
+        $this->client = $client ?? (new Client());
+    }
+
+    /**
+     * @param array $currencies
+     * @return array
+     */
