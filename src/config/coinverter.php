663d260fef6ff9e66dd881f33d8203ad16b643b4
flyinter <120586558+flyinter@users.noreply.github.com>
2022-11-27 20:48:50 +0000

    pricing

--- src/config/coinverter.php
+++ src/config/coinverter.php
@@ -6,3 +6,17 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Drivers
+    |--------------------------------------------------------------------------
+    |
+    | Here you can choose between the different currency conversion API
+    | services that are available.
+    |
+    | Currently available:
+    | 'currencyconverterapi' => https://www.currencyconverterapi.com/
+    | 'exchangeratesapi'     => https://exchangeratesapi.io/
+    |
+    */
+    'driver'               => env('COINVERTER_DRIVER'),
+
+    /*
+    |--------------------------------------------------------------------------
