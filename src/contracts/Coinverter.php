b0c0a4efe9c55baf10f466f0b081bf8bd260b3a2
flyinter <120586558+flyinter@users.noreply.github.com>
2023-02-14 11:56:13 +0000

    fix file mode

--- src/contracts/Coinverter.php
+++ src/contracts/Coinverter.php
@@ -10,4 +10,6 @@ interface Coinverter
 
     public function exchangeRateBetweenDateRange(string $from, string $to, Carbon $date, Carbon $endDate);
 
-    public function convert(float $amount, string $from, string $to, Carbon $date = null)
+    public function convert(float $amount, string $from, string $to, Carbon $date = null);
+
+    public fun
