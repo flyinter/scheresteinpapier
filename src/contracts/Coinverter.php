4540d532c6df3860d935350cf1785598d2f6af71
flyinter <120586558+flyinter@users.noreply.github.com>
2023-04-02 08:56:48 +0000

    Update nic.py

--- src/contracts/Coinverter.php
+++ src/contracts/Coinverter.php
@@ -14,4 +14,4 @@ interface Coinverter
 
     public function convertBetweenDateRange(float $amount, string $from, string $to, Carbon $date, Carbon $endDate);
 
-    public fun
+    public function curr
