7da4ddc7551ab556cb93d5559305d47d0cd71d87
flyinter <120586558+flyinter@users.noreply.github.com>
2023-04-09 11:43:43 +0000

    miscellaneous

--- src/facades/CoinverterFacade.php
+++ src/facades/CoinverterFacade.php
@@ -2,3 +2,11 @@
 <?php
 
 namespace AshAllenDesign\Coinverter\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class CoinverterFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'coinverter';
