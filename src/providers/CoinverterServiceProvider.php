28a19b6c533ae7a7649eda7614d7e77cc5f7fb52
flyinter <120586558+flyinter@users.noreply.github.com>
2023-04-09 20:39:39 +0000

    webapp

--- src/providers/CoinverterServiceProvider.php
+++ src/providers/CoinverterServiceProvider.php
@@ -7,3 +7,11 @@ use AshAllenDesign\Coinverter\CoinverterApiAdapter;
 use AshAllenDesign\Coinverter\Contracts\Coinverter;
 use AshAllenDesign\Coinverter\ExchangeRatesApiAdapter;
 use Illuminate\Support\ServiceProvider;
+
+class CoinverterServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
